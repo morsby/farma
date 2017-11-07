e433e2351624fcba583e39adaeb437d85d887e86
Morsby <sigurd.morsby@gmail.com>
2017-11-07 09:05:53 +0100

    Heroku crash fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
 const keys = require('./config/keys');
 
 mongoose.Promise = global.Promise;
@@ -10,13 +9,24 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/', function(req, res, next) {
-	res.status(200).send('Hi, It works!');
-});
 // use the authRoutes
 require('./routes/apiRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	// Express will serve prod. assets
+	// (js, css files)
+	app.use(express.static('client/build'));
+
+	// Express will serve up index.html if
+	// unknown route
+	// Catchall case, only happens if all above
+	// failed
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
