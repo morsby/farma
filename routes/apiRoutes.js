ee604b0a56585f4fff42337be1446bd137dd690f
Morsby <sigurd.morsby@gmail.com>
2017-11-27 22:22:28 +0100

    Fixed search RegEx

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,17 +7,51 @@ module.exports = app => {
 			.sort({ name: 1 })
 			.select(['name', 'important', 'chapters', 'hasInfo', 'content'])
 			.exec((err, drugs) => {
-				if (err) res.send(err);
-
-				res.json(drugs);
+				if (err) {
+					res.send(err);
+				} else {
+					res.json(drugs);
+				}
 			});
 	});
 
 	app.get('/api/drugs/:drug_id', (req, res) => {
 		Drug.findById(req.params.drug_id).exec((err, drug) => {
-			if (err) res.send(err);
-
-			res.json(drug);
+			if (err) {
+				res.send(err);
+			} else {
+				res.json(drug);
+			}
 		});
+	});
+
+	app.get('/api/drugs/search/:search_term', (req, res) => {
+		if (req.params.search_term) {
+			let searchParam = `"${req.params.search_term.replace(' ', '" "')}"`;
+			//searchParam = req.params.search_term;
+			//// TODO: Search virker ikke...
+			searchParams = req.params.search_term.split(' ');
+			searchParam = '^';
+			searchParams.map(param => {
+				searchParam += `(?=[\\s\\S]*${param}[\\s\\S]*)`;
+			});
+			searchParam += '[\\s\\S]+$';
+
+			console.log(searchParam);
+
+			Drug.find({
+				content: new RegExp(searchParam, 'i')
+			})
+				.select('id')
+				.exec((err, docs) => {
+					if (err) {
+						res.send(err);
+					} else {
+						res.json(docs);
+					}
+				});
+		} else {
+			res.send('No search string given');
+		}
 	});
 };
