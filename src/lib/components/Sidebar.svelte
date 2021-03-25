e68b2031fcffa9bd18dd0edbfdcb5d12df4aa20d
Morsby <sigurd.morsby@gmail.com>
2021-03-25 12:26:25 +0100

    sidebar working

---
+++ src/lib/components/Sidebar.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let sidebarOpen
+</script>
+
+<style> 
+    div#sidebar {
+    @apply fixed z-30 inset-y-0 left-0 w-60 transition duration-300 transform bg-white overflow-y-auto lg:translate-x-0 lg:static lg:inset-0;
+    @apply -translate-x-full ease-in
+   }
+
+   div#sidebar.sidebar-open {
+    @apply translate-x-0 ease-out
+   }
+
+   div#sidebar-title {
+    @apply flex items-center justify-center mt-8;
+    @apply text-gray-800 text-2xl font-semibold;
+   }
+
+   nav#sidebar-nav {
+    @apply flex flex-col mt-10 px-4;
+   }
+</style>
+
+<div id="sidebar" class:sidebar-open={sidebarOpen}>
+    <div id="sidebar-title">
+           SIDEBAR TITLE
+    </div>
+    <nav id="sidebar-nav"> 
+        <slot />
+    </nav>
+</div>
--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,5 +1,35 @@
 <script>
-	import "../global.css";
-</script>
+    let sidebarOpen = false;
+    const toggleSidebar = () => sidebarOpen = !sidebarOpen
 
-<slot />
+	import "../global.css";
+	import Sidebar from '$lib/components/Sidebar.svelte'
+	import Main from '$lib/components/Main.svelte'
+</script>
+ 
+<style>
+div#wrapper {
+	@apply flex h-screen bg-gray-100;
+}
+
+div#overlay {
+	@apply fixed z-20 inset-0 bg-black opacity-50 transition-opacity lg:hidden;
+	display:none;
+}
+ 
+#overlay.sidebar-open {
+	display: block;
+	@apply lg:hidden;
+}
+</style>
+
+<div id="wrapper">
+	<Sidebar sidebarOpen={sidebarOpen}>
+		HER ER TEKSTEN
+	</Sidebar>
+
+    <div id="overlay" class:sidebar-open={sidebarOpen} on:click={toggleSidebar}></div>
+	<Main>
+		<slot />
+	</Main>
+</div>
